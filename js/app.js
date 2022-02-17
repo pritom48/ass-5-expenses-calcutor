4e7b67bb697d635768baa8e3a70e0e8cd5076518
pritom48 <pritamdas41148@gmail.com>
2022-02-17 10:21:50 +0600

    total expenses added

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    //food expenses
+    const foodIput = document.getElementById('food-input');
+    const foodExpenses = parseFloat(foodIput.value);
+    // console.log(foodExpenses)
+
+    // rent expenses
+    const rentInput = document.getElementById('rent-input');
+    const rentExpenses = parseFloat(rentInput.value);
+    // console.log(rentExpenses)
+
+    // clothes expenses
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesExpenses = parseFloat(clothesInput.value);
+    // console.log(clothesExpenses)
+
+    //total expenses
+    const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
+    console.log(totalExpenses);
+})
