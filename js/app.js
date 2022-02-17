caf28588b99bda055d260a56da6cb3154deec57d
pritom48 <pritamdas41148@gmail.com>
2022-02-17 12:55:20 +0600

    savings calculation added

--- js/app.js
+++ js/app.js
@@ -30,14 +30,51 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     const balanceNow = document.getElementById('balance-now');
     balanceNow.innerText = balance;
 
-    // clear all input field
 
-    // clear income input
-    incomeInput.value = '';
-    // clear food input
-    foodInput.value = '';
-    //clear rent input 
-    rentInput.value = '';
-    //clear colthes input 
-    clothesInput.value = '';
+});
+
+
+document.getElementById('savings-btn').addEventListener('click', function () {
+    //income 
+    const incomeInput = document.getElementById('income-input');
+    const income = parseFloat(incomeInput.value);
+
+    //food expenses
+    const foodInput = document.getElementById('food-input');
+    const foodExpenses = parseFloat(foodInput.value);
+    // console.log(foodExpenses)
+
+    // rent expenses
+    const rentInput = document.getElementById('rent-input');
+    const rentExpenses = parseFloat(rentInput.value);
+    // console.log(rentExpenses)
+
+    // clothes expenses
+    const clothesInput = document.getElementById('clothes-input');
+    const clothesExpenses = parseFloat(clothesInput.value);
+    // console.log(clothesExpenses)
+
+    //total expenses
+    const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
+    const totalExpensesField = document.getElementById('total-expenses');
+    totalExpensesField.innerText = totalExpenses;
+
+    //update balance
+    const balance = income - totalExpenses;
+    const balanceNow = document.getElementById('balance-now');
+    balanceNow.innerText = balance;
+
+    //savings calculation
+    const saveInput = document.getElementById('save-input');
+    const savingsPercentage = parseFloat(saveInput.value) / 100;
+    const savingsAmount = income * savingsPercentage;
+
+    const savingsAmountField = document.getElementById('saving-amount-field');
+    savingsAmountField.innerText = savingsAmount;
+
+
+    //remaining balance calculation
+    const remainingBalance = balance - savingsAmount;
+    const remainingBalanceField = document.getElementById('remaining-balance');
+    remainingBalanceField.innerText = remainingBalance;
 })
