a0a4e4127b8e0ea48611a6e3714aef0fbe8d7fca
pritom48 <pritamdas41148@gmail.com>
2022-02-17 13:14:56 +0600

    something added

--- js/app.js
+++ js/app.js
@@ -2,28 +2,28 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
     //income
     const incomeInput = document.getElementById('income-input');
     const income = parseFloat(incomeInput.value);
-    // console.log(income)
+
 
     //food expenses
     const foodInput = document.getElementById('food-input');
     const foodExpenses = parseFloat(foodInput.value);
-    // console.log(foodExpenses)
+
 
     // rent expenses
     const rentInput = document.getElementById('rent-input');
     const rentExpenses = parseFloat(rentInput.value);
-    // console.log(rentExpenses)
+
 
     // clothes expenses
     const clothesInput = document.getElementById('clothes-input');
     const clothesExpenses = parseFloat(clothesInput.value);
-    // console.log(clothesExpenses)
+
 
     //total expenses
     const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
     const totalExpensesField = document.getElementById('total-expenses');
     totalExpensesField.innerText = totalExpenses;
-    // console.log(totalExpenses);
+
 
     //update balance
     const balance = income - totalExpenses;
@@ -42,17 +42,17 @@ document.getElementById('savings-btn').addEventListener('click', function () {
     //food expenses
     const foodInput = document.getElementById('food-input');
     const foodExpenses = parseFloat(foodInput.value);
-    // console.log(foodExpenses)
+
 
     // rent expenses
     const rentInput = document.getElementById('rent-input');
     const rentExpenses = parseFloat(rentInput.value);
-    // console.log(rentExpenses)
+
 
     // clothes expenses
     const clothesInput = document.getElementById('clothes-input');
     const clothesExpenses = parseFloat(clothesInput.value);
-    // console.log(clothesExpenses)
+
 
     //total expenses
     const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
